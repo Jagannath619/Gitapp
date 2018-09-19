bec4c5d8b4bc18557d59ffc46a7378078b10d4d4
Jagannath <>
2018-09-19 12:13:28 +0530

    First commit

---
+++ Gitapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gitapp
+//
+//  Created by Jagannath on 9/19/18.
+//  Copyright © 2018 Jagannath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("New in git")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
